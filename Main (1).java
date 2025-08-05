7a40c0b854b26c5c264e4ba67f299d9c12f92b85
Harini G <harinigovindarajan05@gmail.com>
2025-08-05 19:57:45 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,37 @@
+
+/*import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        int x = Sc.nextInt();
+        int y=Sc.nextInt();
+        for(int i =1;i<=y;i++){
+            System.out.println("y times x"+" ="+x);
+        }
+    }
+}
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        int num1=Sc.nextInt();
+        int i=1;
+        while(i<=10){
+            System.out.println(i*num1+"these are the mutiples of"+num1+"until,10");
+            i++;
+        }
+    }
+}
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        int n=Sc.nextInt();
+        Sc.nextLine();
+        String firstname = Sc.nextLine();
+        String lastname = Sc.nextLine();
+        for(int i=1;i<=n;i++){
+            System.out.println(firstname+lastname);
+        }
+    }
+}*/
