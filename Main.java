ba14f96df9a90dca345844852feae8f3de9dadf0
Harini G <harinigovindarajan05@gmail.com>
2025-08-05 19:05:45 +0530

    Add files via upload
    swithch case if else if else if nested if - assignment 1

---
+++ Main.java
@@ -0,0 +1,91 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+/*import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    int y = sc.nextInt();
+	    sc.nextLine();
+	    String h = sc.nextLine();
+	    char r=sc.next().charAt(0);
+	    System.out.println(y);
+	    System.out.println(h);
+	    System.out.println(r);
+	}
+}
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	      Scanner sc = new Scanner(System.in);
+	      int angle1=sc.nextInt();
+	      int angle2=sc.nextInt();
+	      int angle3=sc.nextInt();
+	      int p=angle1+angle2+angle3;
+	      if(p>=180){
+	          System.out.println("triangle can be formed");
+	      }
+	      else{
+	          System.out.println("cannot be formed");
+	      }
+	}
+}
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	      Scanner sc = new Scanner(System.in);
+	      int mark=sc.nextInt();
+	      if(mark>=90){
+	          System.out.println("grade A");
+	      }
+	      else if(mark>=80){
+	          System.out.println("grade b");
+	      }
+	      else if(mark>=60){
+	          System.out.println("grade c");
+	          
+	      }
+	      else if(mark>=35){
+	          System.out.println("grade d");
+	          
+	      }
+	      else{
+	          System.out.println("fail");
+	      }
+	}
+}
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	      Scanner sc = new Scanner(System.in);
+	      int Size=sc.nextInt();
+	      switch(Size){
+	          case 29:
+	              System.out.println("small");
+	              break;
+	          case 30:
+	              System.out.println("medium");
+	              break;
+	          case 38:
+	              System.out.println("large");
+	              break;
+	          case 42:
+	              System.out.println("XLarge");
+	              break;
+	          default:
+	                System.out.println("XLarge");
+	          
+	      }
+	}
+}*/
+	      
+	      
